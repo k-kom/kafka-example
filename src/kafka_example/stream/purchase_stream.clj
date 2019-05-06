6feb2053f74b32f6189bca971b71956fa00c4205
k-kom <hal02660@gmail.com>
2019-05-06 17:38:15 +0900

    [WIP] purchase stream

--- src/kafka_example/stream/purchase_stream.clj
+++ src/kafka_example/stream/purchase_stream.clj
@@ -7,6 +7,7 @@
            (org.apache.kafka.streams StreamsBuilder
                                      KafkaStreams StreamsConfig)
            (org.apache.kafka.common.serialization Serdes)
+           (org.apache.kafka.clients.consumer ConsumerConfig)
            (java.util Properties)))
 
 (comment
@@ -22,11 +23,70 @@
 ;; preparation
 (def properties
   (doto (Properties.)
-    (.put StreamsConfig/APPLICATION_ID_CONFIG "testing_stream_api")
-    (.put StreamsConfig/BOOTSTRAP_SERVERS_CONFIG "localhost:9092")))
+    (.put StreamsConfig/APPLICATION_ID_CONFIG "testing-stream-api")
+    (.put StreamsConfig/BOOTSTRAP_SERVERS_CONFIG "localhost:9092")
+    (.put ConsumerConfig/AUTO_OFFSET_RESET_CONFIG "latest")
+    (.put StreamsConfig/REPLICATION_FACTOR_CONFIG (int 1))
+    (.put StreamsConfig/CLIENT_ID_CONFIG "Example-Kafka-Streams-Job")
+    (.put ConsumerConfig/GROUP_ID_CONFIG "streams-purchases")))
 
 (def stream-config (StreamsConfig. properties))
 
 (def builder (StreamsBuilder.))
 
 (def string-serde (Serdes/String))
+
+;; returns s with masked string
+(defn- prepend-mask [s]
+  (clojure.string/join "-"
+                       (conj (vec (for [_ (range 3)]
+                                    (apply str (repeat 4 "x"))))
+                             s)))
+
+;; value mappers
+(def mask-credit-card-number
+  (reify ValueMapper
+    (apply [_ {:keys [credit-card-number]}]
+      (let [raw-part (take 12 (clojure.string/replace credit-card-number #"-" ""))]
+        (prepend-mask raw-part)))))
+
+(def purchase->pattern
+  (reify ValueMapper
+    (apply [_ {:keys [zip-code item-purchased purchase-date price quantity]}]
+      {:zip-code zip-code
+       :item     item-purchased
+       :date     purchase-date
+       :amount   (* price quantity)})))
+
+(def purchase->reward
+  (reify ValueMapper
+    (apply [_ {:keys [first-name last-name price quantity]}]
+      (let [t (* price (double quantity))]
+        {:customer-id    (str last-name "," first-name)
+         :purchase-total t
+         :reward-point   (int t)}))))
+
+;; topic definitions
+(def src-stream
+  (.stream builder
+           "source"
+           (Consumed/with string-serde string-serde)))
+
+(def transaction-stream
+  (.. src-stream
+    (mapValues mask-credit-card-number)
+    (to "transactions"
+        (Produced/with string-serde string-serde))))
+
+(def pattern-stream
+  (.. transaction-stream
+    (mapValues purchase->pattern)
+    (to "patterns"
+        (Produced/with string-serde string-serde))))
+
+(def reward-stream
+  (.. purchase->reward
+    (mapValues mask-credit-card-number)
+    (to "rewards"
+        (Produced/with string-serde string-serde))))
+
